168ad14763297637b48915fcea83d6bc87182498
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-29 12:48:39 +0200

    Add files via upload

---
+++ Strategy - HomeWork/Strategy - HomeWork/Strategy - HomeWork.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+enum TimeOfDay { Breakfast, Lunch, Dinner };
+
+enum Product { Bread, Mayonnaise, Dumplings, Cheese, Ham, Egg, Tomato };
+
+class Fridge {
+private:
+    vector<Product> products;
+public:
+    void AddProduct(Product product) {
+        products.push_back(product);
+    }
+
+    bool HasProduct(Product product) const {
+        for (const auto& p : products) {
+            if (p == product) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+class CookingStrategy {
+public:
+    virtual void cook(const Fridge& fridge) const = 0;
+    virtual ~CookingStrategy() = default;
+};
+
+class BreakfastStrategy : public CookingStrategy {
+public:
+    void cook(const Fridge& fridge) const override {
+        if (fridge.HasProduct(Product::Bread) && fridge.HasProduct(Product::Cheese) && fridge.HasProduct(Product::Ham)) {
+            std::cout << "Making a ham and cheese sandwich for breakfast.\n";
+        }
+        else if (fridge.HasProduct(Product::Egg)) {
+            std::cout << "Making scrambled eggs for breakfast.\n";
+        }
+        else {
+            std::cout << "No ingredients for breakfast.\n";
+        }
+    }
+};
+
+class LunchStrategy : public CookingStrategy {
+public:
+    void cook(const Fridge& fridge) const override {
+        if (fridge.HasProduct(Product::Dumplings)) {
+            std::cout << "Cooking dumplings for lunch.\n";
+        }
+        else if (fridge.HasProduct(Product::Tomato) && fridge.HasProduct(Product::Cheese)) {
+            std::cout << "Making a tomato and cheese salad for lunch.\n";
+        }
+        else {
+            std::cout << "No ingredients for lunch.\n";
+        }
+    }
+};
+
+class DinnerStrategy : public CookingStrategy {
+public:
+    void cook(const Fridge& fridge) const override {
+        if (fridge.HasProduct(Product::Bread) && fridge.HasProduct(Product::Mayonnaise) && fridge.HasProduct(Product::Ham)) {
+            std::cout << "Making a ham sandwich for dinner.\n";
+        }
+        else if (fridge.HasProduct(Product::Dumplings)) {
+            std::cout << "Cooking dumplings for dinner.\n";
+        }
+        else {
+            std::cout << "No ingredients for dinner.\n";
+        }
+    }
+};
+
+class CookingContext {
+private:
+    CookingStrategy* strategy = nullptr;
+public:
+    void setStrategy(CookingStrategy* strategy) {
+        this->strategy = strategy;
+    }
+
+    void cook(const Fridge& fridge) const {
+        if (strategy) {
+            strategy->cook(fridge);
+        }
+        else {
+            std::cout << "No cooking strategy set.\n";
+        }
+    }
+};
+
+int main() {
+    Fridge fridge;
+    fridge.AddProduct(Product::Bread);
+    fridge.AddProduct(Product::Cheese);
+    fridge.AddProduct(Product::Ham);
+
+    CookingContext context;
+    TimeOfDay timeOfDay = TimeOfDay::Breakfast;
+   
+   context.setStrategy(new BreakfastStrategy());   
+   /*context.setStrategy(new LunchStrategy());      
+   context.setStrategy(new DinnerStrategy());*/
+   context.cook(fridge);
+}
